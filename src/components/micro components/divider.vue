b3050459f5000fa6beaaab3c4fd4a5f6a0dd9730
DanieleTuttolani <danieletuttolani2@gmail.com>
2023-01-31 14:24:55 +0100

    divider added

---
+++ src/components/micro components/divider.vue
@@ -0,0 +1,12 @@
+<script>
+export default {}
+</script>
+<template>
+    <img src="../../assets/img/divider-white-xx.png" alt="divider">
+</template>
+<style scoped>
+img {
+    filter: invert(27%) sepia(96%) saturate(7495%) hue-rotate(357deg) brightness(103%) contrast(114%);
+    margin: 1rem 0;
+}
+</style>
